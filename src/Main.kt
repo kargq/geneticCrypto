709d7f41c1753685ad5c02d11b7d00c384188b5b
Kindeep <kk17xg@brocku.ca>
2019-10-09 10:46:17 -0400

    tournament selection

--- src/Main.kt
+++ src/Main.kt
@@ -24,71 +24,77 @@ class Main(
 //                )
 //            }
 
-            //            select a new population using a selection strategy
+            // select a new population using a selection strategy
 
-            // Use tournament selection
-            // Pick k members at random then select the best of these ( k = 1, 2 ,.., 5)
-            // Repeat to select more individuals (until population size is reached)
+            applySelectionToPopulation()
 
-            // Whether contestants are picked with replacement
-            // Picking without replacement increases selection pressure
-            // I will pick without replacement.
+            // apply crossover and mutation
+        }
+    }
+
+    // Use tournament selection
+    // Pick k members at random then select the best of these ( k = 1, 2 ,.., 5)
+    // Repeat to select more individuals (until population size is reached)
+
+    // Whether contestants are picked with replacement
+    // Picking without replacement increases selection pressure
+    // I will pick without replacement.
 
 
-            // Whether fittest contestant always wins (deterministic) or this happens with probability p
+    // Whether fittest contestant always wins (deterministic) or this happens with probability p
+    fun applySelectionToPopulation() {
 
-            val new_population: MutableList<Individual> = ArrayList()
+        val new_population: MutableList<Individual> = ArrayList()
 
-            // empty population and create new population
-            while (population.size > 0) {
-                val sample = pickRandomSampleFromPopulation()
-                val weights = ArrayList<Double>(sample.size)
+        // empty population and create new population
+        while (population.size > 0) {
+            val sample = pickRandomSampleFromPopulation()
+            val weights = ArrayList<Double>(sample.size)
 //                var min_fitness = 0.0
 //                var best_in_sample = sample[0]
-                var currSum = 0.0
+            var currSum = 0.0
 
-                var fitnessVals: MutableList<Double> = ArrayList()
-                for (indiv in sample) {
-                    // using fitness values as probabilities for selection, doing a weighted random selection.
+            var fitnessVals: MutableList<Double> = ArrayList()
+            for (indiv in sample) {
+                // using fitness values as probabilities for selection, doing a weighted random selection.
 
-                    val fitness = funcTest.fitness(
-                        indiv.getChromosomeString(),
-                        encryptedString
-                    )
-                    currSum += fitness
-                    weights.add(currSum)
-                    fitnessVals.add(fitness)
+                val fitness = funcTest.fitness(
+                    indiv.getChromosomeString(),
+                    encryptedString
+                )
+                currSum += fitness
+                weights.add(currSum)
+                fitnessVals.add(fitness)
 //                    if (min_fitness < fitness) {
 //                        best_in_sample = indiv
 //                        min_fitness = fitness
 //                    }
-                }
+            }
 
-                val MULTIPLIER = 9999999
+            val MULTIPLIER = 9999999
 
-                var intSum: Int = (currSum * MULTIPLIER).toInt()
+            var intSum: Int = (currSum * MULTIPLIER).toInt()
 
-                var theChosenOne: Individual? = null
+            var theChosenOne: Individual? = null
 //                var theChosenOne: Individual = sample[0]
 
-                for ((index, weight) in weights.withIndex()) {
-                    val adjustedWeight: Int = (weight * MULTIPLIER).toInt()
-                    if (fitnessVals[index] >= adjustedWeight) {
-                        theChosenOne = sample[index]
-                        break
-                    }
+            for ((index, weight) in weights.withIndex()) {
+                val adjustedWeight: Int = (weight * MULTIPLIER).toInt()
+                if (fitnessVals[index] >= adjustedWeight) {
+                    theChosenOne = sample[index]
+                    break
                 }
+            }
 
-                new_population.add(theChosenOne!!)
+            new_population.add(theChosenOne!!)
 //                new_population.add(theChosenOne)
 
 //                new_population.add(best_in_sample)
-            }
-
-            new_population
-//            apply crossover and mutation
         }
+
+        population = new_population
     }
+
 
     fun pickRandomSampleFromPopulation(): List<Individual> {
         val result = ArrayList<Individual>()
@@ -110,7 +116,6 @@ class Main(
         }
         return result
     }
-
 
 }
 
